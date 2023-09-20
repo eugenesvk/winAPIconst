aea6b33b71f8eab5b9efbb04a489e1321a508092
eugenesvk <eugenesvk@users.noreply.github.com>
2023-09-21 00:31:35 +0700

    update imports in build script for testing

--- build.rs
+++ build.rs
@@ -1,3 +1,14 @@
-fn main() {
+#![allow(non_snake_case,non_upper_case_globals,non_camel_case_types,unused_imports,unused_mut,unused_variables,dead_code,unused_assignments,unused_macros)]
 
+extern crate helper;
+use helper        	::*; // gets macros
+use helper::alias 	::*;
+use helper::helper	::*;
+
+use std      	::env;
+use std::fs  	::File;
+use std::io  	::{BufWriter, Write};
+use std::path	::Path;
+
+fn main() {
 }
