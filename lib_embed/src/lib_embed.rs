e18f2f4cea175951df629f2ba53a0ef6897196ff
eugenesvk <eugenesvk@users.noreply.github.com>
2023-10-25 17:04:25 +0700

    __add embed typed support

--- lib_embed/src/lib_embed.rs
+++ lib_embed/src/lib_embed.rs
@@ -1,3 +1,4 @@
 #![allow(non_snake_case,non_upper_case_globals)]
 
-pub mod win_api_const_embed;
+pub mod win_api_const_embed; // library that returns Value
+// pub mod win_api_const_embed_t; // library that returns Type and Value
